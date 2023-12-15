ee7bc69dfb6e2a36f405765cc7b32bc33214a1ea
Theta_Desk <Zhy_theta@outlook.com>
2023-12-15 23:29:36 +0800

    1s闸门信号生成

--- SourceCode/FrequencyMeter.v
+++ SourceCode/FrequencyMeter.v
@@ -2,6 +2,18 @@ module FrequencyMeter (
     input Fxin,
     inout Clk,
     output Frequency
-);
-    reg[]
+    );
+    reg[27:0] Clk_Count;
+    reg Gate_Signal;
+    always @(posedge Clk)   //set 1s gate signal
+    begin
+        if (Clk_Count>=27'd100000000) 
+            begin
+                Clk_Count<=28'b0;
+                Gate_Signal<=~Gate_Signal;
+            end
+        else
+            Clk_Count<=Clk_Count+1'b1;
+        
+    end
 endmodule
